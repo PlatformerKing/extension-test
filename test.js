925a65899a906dceb7fd972a15dbba74458c8566
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-17 09:44:51 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,6 @@
-var main = function() {
-    if(!document.body) {
-        main();
-    }
-    //alert('YAY');
-    //document.body.style.border = "5px solid red";
-    var m = confirm('Unleash Tornado?');
-    if (m) {
-        //function(){
-            var d=document, s = d.createElement("style");
-            s.setAttribute("type","text/css"),d.head.appendChild(s),s.innerHTML="*{-webkit-transform:rotate(99999deg);-ms-transform:rotate(99999deg);transform:rotate(9999deg);transition-duration:30s;}";
-        //})
-    }
+var tornado = function() {
+    var d=document, s = d.createElement("style");
+    s.setAttribute("type","text/css"), d.head.appendChild(s), s.innerHTML="*{-webkit-transform:rotate(99999deg);-ms-transform:rotate(99999deg);transform:rotate(9999deg);transition-duration:30s;}";
 };
-main();
+
 
