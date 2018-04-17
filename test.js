d593ce33b7ac2d70a5ef2ee1b02aa057b6ca8b62
PlatformerKing <PlatformerKing@users.noreply.github.com>
2018-04-17 17:02:02 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var tornado = function() {
     var d=document, s = d.createElement("style");
     s.setAttribute("type","text/css"), d.head.appendChild(s), s.innerHTML="*{-webkit-transform:rotate(99999deg);-ms-transform:rotate(99999deg);transform:rotate(9999deg);transition-duration:30s;}";
-};
+};//Unleashes tornado
 
 
